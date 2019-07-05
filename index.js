3a36b0c0dbfa13bc58c7e6ca45370f3b1f78931c
kouta isikawa <kouta09217@gmail.com>
2019-07-05 11:41:36 +0900

    7/5課題

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // ここにJavaScriptコードを書く
+const tarea = document.getElementById('tarea');
+const input1 = document.getElementById('input1');
+const input2 = document.getElementById('input2')
+const btn = document.getElementById('btn');
+const result = document.getElementById('result');
+
+btn.addEvebtListener('click',()=>{
+    let findtxt = input1.Value;
+    let reptxt = input2.Value;
+    let tagtxt = tarea.value;
+
+    findtxt = new RegExp(findtxt, '9');
+    tagtxt = tagtxt,replace(findtxt, reptxt);
+
+    result.innerText = tagtxt;
+});
