f70001283a8562448734955667b179b6e4c2f953
kouta isikawa <kouta09217@gmail.com>
2019-09-01 15:46:27 +0900

    夏休み　課題

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 // ここにJavaScriptコードを書く
 const tarea = document.getElementById('tarea');
 const input1 = document.getElementById('input1');
-const input2 = document.getElementById('input2')
+const input2 = document.getElementById('input2');
 const btn = document.getElementById('btn');
 const result = document.getElementById('result');
 
 btn.addEvebtListener('click',()=>{
-    let findtxt = input1.Value;
-    let reptxt = input2.Value;
-    let tagtxt = tarea.value;
+  let findtxt = input1.Value;
+  let reptxt = input2.Value;
+  let tagtxt = tarea.value;
 
-    findtxt = new RegExp(findtxt, '9');
-    tagtxt = tagtxt,replace(findtxt, reptxt);
+  findtxt = new RegExp(findtxt, 'g');
+  tagtxt = tagtxt,replace(findtxt, reptxt);
 
-    result.innerText = tagtxt;
+  result.innerText = tagtxt;
 });
